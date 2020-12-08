82fdb1bd4027c1431810882bc468510f72faebb0
Ciro B Rosa <37887021+cirobr@users.noreply.github.com>
2020-12-08 18:06:26 -0300

    Create exercise-5-6.py

---
+++ homework/hw3/exercise-5-6.py
@@ -0,0 +1,87 @@
+# Q5
+
+import pandas as pd
+from collections import Counter
+
+def count_words_fast(text): 
+    text = text.lower() 
+    skips = [".", ",", ";", ":", "'", '"', "\n", "!", "?", "(", ")"] 
+    for ch in skips: 
+        text = text.replace(ch, "") 
+    word_counts = Counter(text.split(" ")) 
+    return word_counts
+
+def summarize_text(language, text):
+    counted_text = count_words_fast(text)
+
+    data = pd.DataFrame({
+        "word": list(counted_text.keys()),
+        "count": list(counted_text.values())
+    })
+    
+    data.loc[data["count"] > 10,  "frequency"] = "frequent"
+    data.loc[data["count"] <= 10, "frequency"] = "infrequent"
+    data.loc[data["count"] == 1,  "frequency"] = "unique"
+    
+    data["length"] = data["word"].apply(len)
+    
+    sub_data = pd.DataFrame({
+        "language": language,
+        "frequency": ["frequent","infrequent","unique"],
+        "mean_word_length": data.groupby(by = "frequency")["length"].mean(),
+        "num_words": data.groupby(by = "frequency").size()
+    })
+    
+    return(sub_data)
+    
+# write your code here!
+hamlets = pd.read_csv("./data/hamlets.csv", sep=",", index_col=(0))
+
+sub_data = pd.DataFrame(columns=["language", "frequency", "mean_word_length", "num_words"])
+for ind in hamlets.index:
+    language, text = hamlets.iloc[ind-1]
+    grouped_data = summarize_text(language, text)
+    sub_data = sub_data.append(grouped_data)
+
+
+# Q6
+colors = {"Portuguese": "green", "English": "blue", "German": "red"}
+markers = {"frequent": "o","infrequent": "s", "unique": "^"}
+import matplotlib.pyplot as plt
+for i in range(grouped_data.shape[0]):
+    row = grouped_data.iloc[i]
+    plt.plot(row.mean_word_length, row.num_words,
+        marker=markers[row.frequency],
+        color = colors[row.language],
+        markersize = 10
+    )
+
+color_legend = []
+marker_legend = []
+for color in colors:
+    color_legend.append(
+        plt.plot([], [],
+        color=colors[color],
+        marker="o",
+        label = color, markersize = 10, linestyle="None")
+    )
+for marker in markers:
+    marker_legend.append(
+        plt.plot([], [],
+        color="k",
+        marker=markers[marker],
+        label = marker, markersize = 10, linestyle="None")
+    )
+plt.legend(numpoints=1, loc = "upper left")
+
+plt.xlabel("Mean Word Length")
+plt.ylabel("Number of Words")
+# write your code to display the plot here!
+
+for i in range(sub_data.shape[0]):
+    row = sub_data.iloc[i]
+    plt.plot(row.mean_word_length, row.num_words,
+        marker=markers[row.frequency],
+        color = colors[row.language],
+        markersize = 10
+    )
