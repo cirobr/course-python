35f82aa54c63a4e9679e827020c4dc38590bde13
Ciro B Rosa <37887021+cirobr@users.noreply.github.com>
2021-01-18 14:29:07 -0300

    hw3-3

---
+++ homework/hw3/hw3-3.py
@@ -0,0 +1,54 @@
+import numpy as np, random, scipy.stats as ss
+
+def majority_vote_fast(votes):
+    mode, count = ss.mstats.mode(votes)
+    return mode
+
+def distance(p1, p2):
+    return np.sqrt(np.sum(np.power(p2 - p1, 2)))
+
+def find_nearest_neighbors(p, points, k=5):
+    distances = np.zeros(points.shape[0])
+    for i in range(len(distances)):
+        distances[i] = distance(p, points[i])
+    ind = np.argsort(distances)
+    return ind[:k]
+
+def knn_predict(p, points, outcomes, k=5):
+    ind = find_nearest_neighbors(p, points, k)
+    return majority_vote_fast(outcomes[ind])[0]
+
+# e1
+import pandas as pd
+folder = "./data/"
+file = "wine.csv"
+fullname = folder + file
+df = pd.read_csv(fullname)
+
+df_temp = df.head(5)
+print(np.sum(df_temp.high_quality == 1))
+print("\n")
+
+# e2
+numeric_data = df
+is_red = []
+for c in numeric_data.color:
+    if c == "red":
+        is_red.append(1)
+    elif c == "white":
+        is_red.append(0)
+
+numeric_data["is_red"] = is_red
+numeric_data = numeric_data.drop(columns=["color", "quality", "high_quality"])
+
+print(np.sum(numeric_data.is_red))
+print("\n")
+
+# e3
+from sklearn import preprocessing
+scaled_data = preprocessing.scale(numeric_data)
+numeric_data = pd.DataFrame(scaled_data, columns = numeric_data.columns)
+
+from sklearn import decomposition
+pca = decomposition.PCA()
+principal_components = pca.fit_transform(numeric_data)
