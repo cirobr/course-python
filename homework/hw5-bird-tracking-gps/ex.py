fe42177c70974fe86908c9f638c4b0fe1f50804f
cirobr <ciro.rosa@alumni.usp.br>
2021-01-29 11:04:45 -0300

    Update ex.py

--- homework/hw5-bird-tracking-gps/ex.py
+++ homework/hw5-bird-tracking-gps/ex.py
@@ -6,7 +6,19 @@ print(birddata.head())
 import matplotlib.pyplot as plt
 import numpy as np
 
-ix = birddata.bird_name == "Eric"
-x, y = birddata.longitude[ix], birddata.latitude[ix]
 plt.figure(figsize=(7,7))
-plt.plot(x,y)
+bird_names = pd.unique(birddata.bird_name)
+for bird_name in bird_names:
+    ix = (birddata.bird_name == bird_name)
+    x, y = birddata.longitude[ix], birddata.latitude[ix]
+    plt.plot(x, y, ".", label=bird_name)
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.legend(loc="lower right")
+plt.show()
+
+ix = (birddata.bird_name == "Eric")
+speed = birddata.speed_2d[ix]
+#ind = np.isnan(speed)
+#ind1 = ~ind
+plt.hist(speed, bins=np.linspace(0, 30, 20))
