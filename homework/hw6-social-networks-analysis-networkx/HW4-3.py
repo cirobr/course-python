85aa8f21ec913dfe3c5b24be88078ce9328bed70
Ciro B Rosa <37887021+cirobr@users.noreply.github.com>
2021-02-08 13:49:33 -0300

    hw6.2

--- homework/hw6-social-networks-analysis-networkx/HW4-3.py
+++ homework/hw6-social-networks-analysis-networkx/HW4-3.py
@@ -69,3 +69,21 @@ max_key = max(chances, key=chances.get)
 print(max_key)
 
 # q5
+def homophily(G, chars, IDs):
+    """
+    Given a network G, a dict of characteristics chars for node IDs,
+    and dict of node IDs for each node in the network,
+    find the homophily of the network.
+    """
+    num_same_ties = 0
+    num_ties = 0
+    for n1, n2 in G.edges():
+        if IDs[n1] in chars and IDs[n2] in chars:
+            if G.has_edge(n1, n2):
+                # Should `num_ties` be incremented?  What about `num_same_ties`?
+                num_ties += 1
+                if chars[IDs[n1]] == chars[IDs[n2]]:
+                    # Should `num_ties` be incremented?  What about `num_same_ties`?
+                    num_same_ties += 1
+    return (num_same_ties / num_ties)
+
